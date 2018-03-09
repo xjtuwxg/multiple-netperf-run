5ff2efbe63591a14d1eb1e616dce8cae3acb48bf
xgwang <xjtuwxg@gmail.com>
2018-03-09 15:56:52 +0800

    Update tcp_rr.sh

--- tcp_rr.sh
+++ tcp_rr.sh
@@ -10,10 +10,9 @@ EXEC_TIME=$2
 
 echo "Server IP: " $SERVER_IP  "Exec time:" $EXEC_TIME "seconds for each test case"
 
-TEST_RESULT_NAME="tcp_rr.result"
+TEST_CASE="tcp_rr"
+TEST_RESULT_NAME=$TEST_CASE".result"
 BIN=./netperf-2.6.0/src/netperf
-#SIGNATURE='16384 '
-
 
 for cnt in 1 5 20 100 400
 do
@@ -22,10 +21,10 @@ do
         echo > $TEST_RESULT_NAME$cnt
 
         for ((i=1; i <= $cnt; i++)); do
-                $BIN -H $SERVER_IP -l $EXEC_TIME -t tcp_rr -- -r 100,100 >> $TEST_RESULT_NAME$cnt &
+                $BIN -H $SERVER_IP -l $EXEC_TIME -t $TEST_CASE -- -r 100,100 >> $TEST_RESULT_NAME$cnt &
         done
         wait
 
         echo "Write result to" $TEST_RESULT_NAME$cnt
-        cat $TEST_RESULT_NAME$cnt | grep "100      100" | awk '{print $6}' | awk '{sum+=$1} END {print "Netperf stream number =", NR, ", sum =", sum} '
+        cat $TEST_RESULT_NAME$cnt | grep " 100 " | awk '{print $6}' | awk '{sum+=$1} END {print "Netperf stream number =", NR, ", sum =", sum} '
 done
